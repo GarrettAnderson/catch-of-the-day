e8499643144390100679584aef68254b12f23f75
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-03 18:28:08 -0400

    created the storepicker component and getting the app set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+render(<StorePicker/>, document.querySelector('#root'))
