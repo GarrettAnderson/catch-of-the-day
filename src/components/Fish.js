d6689dee699bc1c591cbb61ae6b0d3ce6c4b68c5
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-04 18:08:42 -0400

    added price variable and other variables to be destructured from the details props. created helpers file to get the proper price format

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 class Fish extends React.Component {
   render() {
-    const image = this.props.details.image
-    const name = this.props.details.name
+    const { image, name, price, description, status } = this.props.details
 
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
-        <h3 className="fish-name">{name}</h3>
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{price}</span>
+        </h3>
       </li>
     )
   }
