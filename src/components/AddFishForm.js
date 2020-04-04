487a735e260cea0617187893846c7fc4e6d3d67b
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-04 16:31:57 -0400

    added refs to the name price status and image input elements

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,23 +1,24 @@
 import React from 'react'
 
 class AddFishForm extends React.Component {
-  createFish(event) {
+  createFish = (event) => {
     // 1. Stop the form from submitting
     event.preventDefault()
     console.log('Making a fish!')
+    //
   }
 
   render() {
     return (
       <form className="fish-edit" onSubmit={this.createFish}>
-        <input name="name" type="text" placeholder="Name" />
-        <input name="price" type="text" placeholder="Price" />
-        <select name="status">
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
         <textarea name="desc" placeholder="Desc" />
-        <input name="image" type="text" placeholder="Image" />
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
         <button type="submit">+ Add Fish</button>
       </form>
     )
