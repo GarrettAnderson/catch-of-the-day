62ca83069564330c5394017dcc27d4bd82741105
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-04 18:32:31 -0400

    working on adding to order functionality

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { formatPrice } from '../helpers'
 
 class Fish extends React.Component {
-  render() {
-    const { image, name, price, description, status } = this.props.details
+  handleClick = () => {
+    this.props.addToOrder(this.props.index)
+  }
 
+  render() {
+    const { image, name, price, desc, status } = this.props.details
+    const isAvailable = status === 'available'
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,6 +16,10 @@ class Fish extends React.Component {
           {name}
           <span className="price">{formatPrice(price)}</span>
         </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add To Cart' : 'Sold Out!'}
+        </button>
       </li>
     )
   }
