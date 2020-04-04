3a0e8ce214ac0e672ea2f342da28fa301cf8a682
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-04 16:04:44 -0400

    added form element and input textarea and select elements along with the onSubmit inline call and added event.preventDefault

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 class AddFishForm extends React.Component {
+  createFish(event) {
+    // 1. Stop the form from submitting
+    event.preventDefault()
+    console.log('Making a fish!')
+  }
+
   render() {
-    return <div />
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" placeholder="Desc" />
+        <input name="image" type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
   }
 }
 
